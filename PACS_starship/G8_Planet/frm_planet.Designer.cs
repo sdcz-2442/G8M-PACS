44dc648195bdd604a9d1aaf3e9f9b8338f577556
Yeyizo <ericmoremolao@gmail.com>
2021-03-11 19:26:10 +0100

    Cambios diseño frm planeta

--- PACS_starship/G8_Planet/frm_planet.Designer.cs
+++ PACS_starship/G8_Planet/frm_planet.Designer.cs
@@ -46,8 +46,19 @@ namespace G8_Planet
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.lbl_port = new System.Windows.Forms.Panel();
+            this.minimize_click = new System.Windows.Forms.PictureBox();
+            this.maximize_click = new System.Windows.Forms.PictureBox();
+            this.close_click = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.groupBox1.SuspendLayout();
             this.pnl_keys.SuspendLayout();
+            this.lbl_port.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minimize_click)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maximize_click)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close_click)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -58,11 +69,11 @@ namespace G8_Planet
             this.groupBox1.Controls.Add(this.btn_connect);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(11, 96);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Location = new System.Drawing.Point(20, 249);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(434, 296);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(579, 364);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Enviament dades";
@@ -70,35 +81,37 @@ namespace G8_Planet
             // lbx_Missatges
             // 
             this.lbx_Missatges.FormattingEnabled = true;
-            this.lbx_Missatges.Location = new System.Drawing.Point(58, 147);
+            this.lbx_Missatges.ItemHeight = 16;
+            this.lbx_Missatges.Location = new System.Drawing.Point(77, 181);
+            this.lbx_Missatges.Margin = new System.Windows.Forms.Padding(4);
             this.lbx_Missatges.Name = "lbx_Missatges";
-            this.lbx_Missatges.Size = new System.Drawing.Size(328, 134);
+            this.lbx_Missatges.Size = new System.Drawing.Size(436, 164);
             this.lbx_Missatges.TabIndex = 5;
             // 
             // txb_port
             // 
-            this.txb_port.Location = new System.Drawing.Point(58, 36);
-            this.txb_port.Margin = new System.Windows.Forms.Padding(2);
+            this.txb_port.Location = new System.Drawing.Point(77, 44);
+            this.txb_port.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txb_port.Name = "txb_port";
-            this.txb_port.Size = new System.Drawing.Size(63, 20);
+            this.txb_port.Size = new System.Drawing.Size(83, 22);
             this.txb_port.TabIndex = 4;
             // 
             // btn_desconnect
             // 
-            this.btn_desconnect.Location = new System.Drawing.Point(289, 33);
-            this.btn_desconnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_desconnect.Location = new System.Drawing.Point(385, 41);
+            this.btn_desconnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_desconnect.Name = "btn_desconnect";
-            this.btn_desconnect.Size = new System.Drawing.Size(86, 20);
+            this.btn_desconnect.Size = new System.Drawing.Size(115, 25);
             this.btn_desconnect.TabIndex = 3;
             this.btn_desconnect.Text = "Desconnectar";
             this.btn_desconnect.UseVisualStyleBackColor = true;
             // 
             // btn_connect
             // 
-            this.btn_connect.Location = new System.Drawing.Point(196, 33);
-            this.btn_connect.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_connect.Location = new System.Drawing.Point(261, 41);
+            this.btn_connect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_connect.Name = "btn_connect";
-            this.btn_connect.Size = new System.Drawing.Size(88, 20);
+            this.btn_connect.Size = new System.Drawing.Size(117, 25);
             this.btn_connect.TabIndex = 2;
             this.btn_connect.Text = "Connectar";
             this.btn_connect.UseVisualStyleBackColor = true;
@@ -106,20 +119,18 @@ namespace G8_Planet
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 113);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(75, 139);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.Size = new System.Drawing.Size(115, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Missatges rebuts";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 36);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(37, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.Size = new System.Drawing.Size(34, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Port";
             // 
@@ -132,16 +143,18 @@ namespace G8_Planet
             this.pnl_keys.Controls.Add(this.tbx_container);
             this.pnl_keys.Controls.Add(this.label3);
             this.pnl_keys.Controls.Add(this.label4);
-            this.pnl_keys.Location = new System.Drawing.Point(464, 12);
+            this.pnl_keys.Location = new System.Drawing.Point(624, 146);
+            this.pnl_keys.Margin = new System.Windows.Forms.Padding(4);
             this.pnl_keys.Name = "pnl_keys";
-            this.pnl_keys.Size = new System.Drawing.Size(502, 148);
+            this.pnl_keys.Size = new System.Drawing.Size(669, 182);
             this.pnl_keys.TabIndex = 19;
             // 
             // btn_generate
             // 
-            this.btn_generate.Location = new System.Drawing.Point(337, 106);
+            this.btn_generate.Location = new System.Drawing.Point(449, 130);
+            this.btn_generate.Margin = new System.Windows.Forms.Padding(4);
             this.btn_generate.Name = "btn_generate";
-            this.btn_generate.Size = new System.Drawing.Size(93, 23);
+            this.btn_generate.Size = new System.Drawing.Size(124, 28);
             this.btn_generate.TabIndex = 7;
             this.btn_generate.Text = "Generar Claus";
             this.btn_generate.UseVisualStyleBackColor = true;
@@ -149,9 +162,10 @@ namespace G8_Planet
             // 
             // btn_routeXML
             // 
-            this.btn_routeXML.Location = new System.Drawing.Point(436, 78);
+            this.btn_routeXML.Location = new System.Drawing.Point(581, 96);
+            this.btn_routeXML.Margin = new System.Windows.Forms.Padding(4);
             this.btn_routeXML.Name = "btn_routeXML";
-            this.btn_routeXML.Size = new System.Drawing.Size(34, 23);
+            this.btn_routeXML.Size = new System.Drawing.Size(45, 28);
             this.btn_routeXML.TabIndex = 6;
             this.btn_routeXML.Text = "...";
             this.btn_routeXML.UseVisualStyleBackColor = true;
@@ -159,78 +173,165 @@ namespace G8_Planet
             // 
             // tbx_routeXML
             // 
-            this.tbx_routeXML.Location = new System.Drawing.Point(142, 80);
+            this.tbx_routeXML.Location = new System.Drawing.Point(189, 98);
+            this.tbx_routeXML.Margin = new System.Windows.Forms.Padding(4);
             this.tbx_routeXML.Name = "tbx_routeXML";
-            this.tbx_routeXML.Size = new System.Drawing.Size(288, 20);
+            this.tbx_routeXML.Size = new System.Drawing.Size(383, 22);
             this.tbx_routeXML.TabIndex = 5;
             // 
             // tbx_container
             // 
-            this.tbx_container.Location = new System.Drawing.Point(142, 36);
+            this.tbx_container.Location = new System.Drawing.Point(189, 44);
+            this.tbx_container.Margin = new System.Windows.Forms.Padding(4);
             this.tbx_container.Name = "tbx_container";
-            this.tbx_container.Size = new System.Drawing.Size(127, 20);
+            this.tbx_container.Size = new System.Drawing.Size(168, 22);
             this.tbx_container.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 83);
+            this.label3.Location = new System.Drawing.Point(39, 102);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.Size = new System.Drawing.Size(140, 17);
             this.label3.TabIndex = 3;
             this.label3.Text = "Fixer XML Public Key";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(41, 39);
+            this.label4.Location = new System.Drawing.Point(55, 48);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.Size = new System.Drawing.Size(126, 17);
             this.label4.TabIndex = 2;
             this.label4.Text = "Nom KeyContainer";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(42, 22);
+            this.comboBox1.Location = new System.Drawing.Point(70, 156);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(363, 21);
+            this.comboBox1.Size = new System.Drawing.Size(483, 24);
             this.comboBox1.TabIndex = 20;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(70, 46);
+            this.button1.Location = new System.Drawing.Point(211, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.Size = new System.Drawing.Size(167, 28);
             this.button1.TabIndex = 21;
             this.button1.Text = "Seleccionar Planeta";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(247, 46);
+            this.button2.Location = new System.Drawing.Point(386, 188);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.Size = new System.Drawing.Size(167, 28);
             this.button2.TabIndex = 22;
             this.button2.Text = "Generar identificador";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // lbl_port
+            // 
+            this.lbl_port.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.lbl_port.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(6)))), ((int)(((byte)(14)))));
+            this.lbl_port.Controls.Add(this.minimize_click);
+            this.lbl_port.Controls.Add(this.maximize_click);
+            this.lbl_port.Controls.Add(this.close_click);
+            this.lbl_port.Controls.Add(this.panel3);
+            this.lbl_port.Controls.Add(this.panel2);
+            this.lbl_port.Controls.Add(this.flowLayoutPanel2);
+            this.lbl_port.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl_port.Location = new System.Drawing.Point(0, 0);
+            this.lbl_port.Name = "lbl_port";
+            this.lbl_port.Size = new System.Drawing.Size(1344, 28);
+            this.lbl_port.TabIndex = 23;
+            // 
+            // minimize_click
+            // 
+            this.minimize_click.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimize_click.Location = new System.Drawing.Point(1278, 4);
+            this.minimize_click.Name = "minimize_click";
+            this.minimize_click.Size = new System.Drawing.Size(15, 15);
+            this.minimize_click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.minimize_click.TabIndex = 17;
+            this.minimize_click.TabStop = false;
+            // 
+            // maximize_click
+            // 
+            this.maximize_click.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.maximize_click.Location = new System.Drawing.Point(1299, 4);
+            this.maximize_click.Name = "maximize_click";
+            this.maximize_click.Size = new System.Drawing.Size(15, 15);
+            this.maximize_click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.maximize_click.TabIndex = 16;
+            this.maximize_click.TabStop = false;
+            // 
+            // close_click
+            // 
+            this.close_click.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.close_click.Location = new System.Drawing.Point(1323, 4);
+            this.close_click.Name = "close_click";
+            this.close_click.Size = new System.Drawing.Size(15, 15);
+            this.close_click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.close_click.TabIndex = 14;
+            this.close_click.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(191)))), ((int)(((byte)(141)))));
+            this.panel3.Location = new System.Drawing.Point(1, 14);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(650, 1);
+            this.panel3.TabIndex = 15;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(191)))), ((int)(((byte)(141)))));
+            this.panel2.Location = new System.Drawing.Point(-1, 8);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(750, 2);
+            this.panel2.TabIndex = 14;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(191)))), ((int)(((byte)(141)))));
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(850, 4);
+            this.flowLayoutPanel2.TabIndex = 13;
+            // 
             // frm_planet
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1344, 652);
+            this.Controls.Add(this.lbl_port);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pnl_keys);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_planet";
             this.Text = "frm_planet";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.pnl_keys.ResumeLayout(false);
             this.pnl_keys.PerformLayout();
+            this.lbl_port.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.minimize_click)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maximize_click)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close_click)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -254,5 +355,12 @@ namespace G8_Planet
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel lbl_port;
+        private System.Windows.Forms.PictureBox minimize_click;
+        private System.Windows.Forms.PictureBox maximize_click;
+        private System.Windows.Forms.PictureBox close_click;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
     }
 }
